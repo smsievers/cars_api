831f454466b2d757e6db52d525b5e41fb89dad16
smsievers <seth_sievers@yahoo.com>
2020-08-17 13:48:28 -0500

    User services in place

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::UsersController < ApplicationController
+    skip_before_action :authenticate, only: [:login, :create]
+
+    def login 
+        render json: {error: "User not authenticated."}, status: 401 and return unless UsersService.login(params[:email], params[:password])
+        render json: @user.profile status: :ok
+    end
+
+    def create
+        @user = UsersService.register(params[:email], params[:first_name], params[:last_name], params[:nickname], params[:password], params[:password_confirmation])
+        render json: {error: "There was a problem saving your user."}, status: :unprocessable_entry and return unless @user
+        render json: @user.profile, status: :ok
+    end
+
+    def logout
+        render json: {error: "There was a problem logging you out."}, status: :unprocessable_entry and return unless UsersService.logout(current_user)
+        render json: {success: "You have been logged out."}, status: :ok 
+    end
+
+    def me
+        render json: @current_user.profile, status: :ok
+    end
+end
